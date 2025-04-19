3631be6576d1fc88235d1f5c1df4457b09ec2741
Pratyush <foreverwillin1@gmail.com>
2025-04-20 00:14:10 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Community Pipeline: A modular Python package for hybrid community detection.
+
+This package implements a hybrid community detection approach using:
+- Louvain method for initial community detection
+- Girvan-Newman refinement for large communities
+- Infomap enhancement for low-modularity communities
+
+Designed for the SNAP LiveJournal dataset but can be adapted for other networks.
+"""
+
+__version__ = '0.1.0'
